e2b1bb3ad9abf43ac1aa5efe1986db9962c42adf
Lucas Marchiori <sinonsrt@hotmail.com>
2023-10-26 16:27:44 -0300

    feat: config dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from './app';
+import { env } from './config/env';
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
-  .then(() => console.log('Server up 🚀'));
+  .then(() => console.log(`Server up at ${env.PORT} 🚀`));
